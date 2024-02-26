9ab57d17a71ed053a6e4492db519495efd4d2029
arielhernandezcl <ariel.dox@gmail.com>
2024-02-26 18:29:32 -0300

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 const API_KEY = "4905856019427443/"
-//const BASE_URL = "https://superhero.arielhernandezcl.workers.dev/"
 const BASE_URL = "https://fixcors.site/https://www.superheroapi.com/api/"+API_KEY;
-
 let chart;
 
 window.onload = function() {
@@ -27,55 +25,60 @@ window.onload = function() {
     chart.render();
   }
 
-function handleButtonClick(e) {
+  function handleButtonClick(e) {
     e.preventDefault();
-  
+    
     const inputValue = $("#buscar").val();
-    const regex = /^[0-9]+$/;
-
-    if (!regex.test(inputValue)) {
-        alert("Solo numeros permitidos");
-        return; 
+    const numero = parseInt(inputValue);
+    
+    if (isNaN(numero)) {
+        alert("Por favor, ingresa solo números.");
+        return;
     }
+
+    if (numero < 1 || numero > 731) {
+        alert("Por favor, ingresa un número entre 1 y 731.");
+    } else {
+
+      $.ajax({
+        type: "GET",
+        url: BASE_URL + inputValue,
+        success: function (result) {
+          console.log(result);
+          $("#main").hide();
+          $("#results").removeAttr("hidden");
+          $(".rounded-start").attr("src", result.image.url);
+          $(".rounded-start").attr("alt", result.name);
+          $(".card-title").html("<b>Nombre</b>: " + result.name);
+          $(".card-text-1").html("<b>Conexiones</b>: " + result.connections["group-affiliation"]);
+          $(".card-text-2").html("<b>Ocupacion</b>: " + result.work["occupation"]);
+          $("#desc1").html("<b>1° Aparicion</b>: " + result.biography["first-appearance"]);
+          $("#desc2").html("<b>Altura</b>: " + result.appearance["height"][1]);
+          $("#desc3").html("<b>Peso</b>: " + result.appearance["weight"][1]);
+          $("#desc4").html("<b>Publicado Por</b>: " + result.biography.publisher);
   
-    $.ajax({
-      type: "GET",
-      url: BASE_URL + inputValue,
-      success: function (result) {
-        console.log(result);
-        $("#main").hide();
-        $("#results").removeAttr("hidden");
-        $(".rounded-start").attr("src", result.image.url);
-        $(".rounded-start").attr("alt", result.name);
-        $(".card-title").html("<b>Nombre</b>: " + result.name);
-        $(".card-text-1").html("<b>Conexiones</b>: " + result.connections["group-affiliation"]);
-        $(".card-text-2").html("<b>Ocupacion</b>: " + result.work["occupation"]);
-        $("#desc1").html("<b>1° Aparicion</b>: " + result.biography["first-appearance"]);
-        $("#desc2").html("<b>Altura</b>: " + result.appearance["height"][1]);
-        $("#desc3").html("<b>Peso</b>: " + result.appearance["weight"][1]);
-        $("#desc4").html("<b>Publicado Por</b>: " + result.biography.publisher);
-
-        const aliases = result.biography.aliases;
-          const aliasList = $("<ul></ul>");
-          aliases.forEach(function(alias) {
-            const listItem = $("<li></li>").text(alias);
-            aliasList.append(listItem);
-          });
-          $("#aliases-container").html(aliasList);
-
-          const powerstats = result.powerstats;
-          const powerstatNames = Object.keys(powerstats);
-          const powerstatValues = Object.values(powerstats);
-          const dataPoints = powerstatNames.map((name, index) => ({ label: name, y: powerstatValues[index] }));
-
-          chart.options.data[0].dataPoints = dataPoints;
-          chart.options.title.text = "Estadísticas de poder de: " + result.name;
-          chart.render();
-      },
-      error: function (result) {
-        alert("Error");
-      },
-    });
+          const aliases = result.biography.aliases;
+            const aliasList = $("<ul></ul>");
+            aliases.forEach(function(alias) {
+              const listItem = $("<li></li>").text(alias);
+              aliasList.append(listItem);
+            });
+            $("#aliases-container").html(aliasList);
+  
+            const powerstats = result.powerstats;
+            const powerstatNames = Object.keys(powerstats);
+            const powerstatValues = Object.values(powerstats);
+            const dataPoints = powerstatNames.map((name, index) => ({ label: name, y: powerstatValues[index] }));
+  
+            chart.options.data[0].dataPoints = dataPoints;
+            chart.options.title.text = "Estadísticas de poder de: " + result.name;
+            chart.render();
+        },
+        error: function (result) {
+          alert("Error");
+        },
+      });
+    }
 }
 
 $("button").click(handleButtonClick);
